a54d564bf7dc8a6b1a8b7a015da6ab63329e0d58
vdk0987 <vdk0987@gmail.com>
2026-08-28 20:11:15 +0530

    Establish schedlue config

---
+++ packages/engine/types.js
@@ -0,0 +1,14 @@
+export const SCHEDULING_CONFIG = {
+  DAYS: [1, 2, 3, 4],
+
+  DAY_START_MINUTES: 9 * 60,  // 09:00
+  DAY_END_MINUTES: 17 * 60,   // 17:00
+
+  TICK_MINUTES: 15,
+
+  INTERVIEW_STATUS: {
+    SCHEDULED: "scheduled",
+    UNSCHEDULED: "unscheduled",
+    WITHDRAWN: "withdrawn",
+  },
+};
