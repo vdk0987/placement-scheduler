33e81979bc92d60594d8f82f752551cf58eca9ad
vdk0987 <vdk0987@gmail.com>
2026-08-28 20:50:19 +0530

    refactor create interview requests

--- packages/engine/helper/createInterviewRequest.js
+++ packages/engine/helper/createInterviewRequest.js
@@ -1,4 +1,29 @@
-import { buildIndexes } from "./buildIndexes.js";
+function buildIndexes(dataset) {
+  const companiesById = new Map(
+    dataset.companies.map((company) => [company.id, company]),
+  );
+
+  const studentsById = new Map(
+    dataset.students.map((student) => [student.id, student]),
+  );
+
+  const panelsById = new Map();
+
+  for (const company of dataset.companies) {
+    for (const panel of company.panels) {
+      panelsById.set(panel.id, panel);
+    }
+  }
+
+  const roomsById = new Map(dataset.rooms.map((room) => [room.id, room]));
+
+  return {
+    companiesById,
+    studentsById,
+    panelsById,
+    roomsById,
+  };
+}
 
 export function createInterviewRequests(dataset) {
   const requests = [];
